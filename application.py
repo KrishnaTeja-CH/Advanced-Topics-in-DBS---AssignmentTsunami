c71352cf2a41a273669896de937552a9fa4068ce
KrishnaTeja-Dot <kxc1534@mavs.uta.edu>
2021-07-01 17:10:13 -0500

    123

--- application.py
+++ application.py
@@ -1,95 +1,63 @@
 from flask import Flask, render_template, request, url_for
 import os
+import string
 
 
 application = Flask(__name__)
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-words1 = os.path.join(basedir, 'static/AliceCleaneredit.txt')
-words2 = os.path.join(basedir, 'static/AliceInWonderlandedit.txt')
-words3 = os.path.join(basedir, 'static/CandideEnedit.txt')
-words4 = os.path.join(basedir, 'static/CandideFredit.txt')
+word_file = os.path.join(basedir, 'static/doc1.txt')
+
 
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
-    print("hello there")
-    return render_template('index.html')
-
-
-@application.route('/searchword', methods=['GET', 'POST'])
-def search_word():
     word1 = []
-    word2 = []
-    word3 = []
-    word4 = []
-    files = []
-    wordss1 = []
-    wordss2 = []
-    wordss3 = []
-    wordss4 = []
-
+    text_file = ""
+    number_of_words = 0
     if request.method == 'POST':
-        query_word = request.form.get('words')
+        word = request.form.get('words')
+        filename = request.form.get('filename')
 
-        with open(words1, 'rb') as fileinput:
+        word = word.lower()
+        word = [''.join(c for c in s if c not in string.punctuation) for s in word]
+        word = "".join(word)
+        words_file = os.path.join(basedir, 'static/' + filename)
+        with open(words_file, "a") as output:
+            output.write(str(word))
+
+        with open(word_file, "a") as output:
+            output.write(str(word))
+
+        text_file = filename
+        with open(word_file, 'rb') as fileinput:
             for line in fileinput:
                 for words in line.split():
-                    word1.append(str(words))
-        for values in word1:
-            wordss1.append(values[2:-1])
+                    word1.append(str(words)[2:-1])
 
-        print(wordss1)
+        number_of_words = len(word1)
 
-        if query_word in wordss1:
-            movie_title1 = "AliceCleaneredit.txt"
-            files.append(movie_title1)
+    return render_template('index.html', textfile = text_file, number = number_of_words)
 
-        with open(words2, 'rb') as fileinput:
+
+@application.route('/searchoccurances', methods = ['GET', 'POST'])
+def search_occurances():
+    wordss = []
+    if request.method == 'POST':
+        word_entered = request.form.get('word')
+        with open(word_file, 'rb') as fileinput:
             for line in fileinput:
                 for words in line.split():
-                    word2.append(str(words))
-
-        for values in word2:
-            wordss2.append(values[2:-1])
-
-        print(wordss2)
-
-        if query_word in wordss2:
-            movie_title2 = "AliceInWonderlandedit.txt"
-            files.append(movie_title2)
-
-        with open(words3, 'rb') as fileinput:
-            for line in fileinput:
-                for words in line.split():
-                    word3.append(str(words))
-
-        for values in word3:
-            wordss3.append(values[2:-1])
-
-        print(wordss3)
-
-        if query_word in wordss3:
-            movie_title3 = "CandideEnedit.txt"
-            files.append(movie_title3)
-
-        with open(words4, 'rb') as fileinput:
-            for line in fileinput:
-                for words in line.split():
-                    word4.append(str(words))
-
-        for values in word4:
-            wordss4.append(values[2:-1])
-
-        print(wordss4)
-
-        if query_word in wordss4:
-            movie_title4 = "CandideFredit.txt"
-            files.append(movie_title4)
-
-    return render_template("search_word.html", file = files)
+                    wordss.append(str(words)[2:-1])
+        wordss_line = " ".join(wordss)
+        print(wordss_line)
+        res = [i for i in range(len(wordss_line)) if wordss_line.startswith(word_entered, i)]
+        print(res)
+        print(len(res))
+        total_length = len(res)
+    return render_template('index.html', occu = total_length)
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     application.run()
